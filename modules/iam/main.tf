fe6654c6e473de9fd3959007f11bbea0a695ba59
priyank-139 <114915047+priyank-139@users.noreply.github.com>
2022-12-07 17:29:53 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -31,7 +31,7 @@ resource "aws_iam_role" "vault-automation" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-          Service = "kms" 
+          Service = "ec2.amazonaws.com" 
         }
       },
     ]
